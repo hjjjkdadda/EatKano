3223907e5aa22cf48d0f88c8e76623cb160821c8
hjjjkdadda <115965552+hjjjkdadda@users.noreply.github.com>
2022-11-04 15:59:43 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -182,15 +182,15 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
 
     function gameInit() {
         createjs.Sound.registerSound({
-            src: "./static/music/err.mp3",
+            src: "./static/music/err.Mp3",
             id: "err"
         });
         createjs.Sound.registerSound({
-            src: "./static/music/end.mp3",
+            src: "./static/music/end.Mp3",
             id: "end"
         });
         createjs.Sound.registerSound({
-            src: "./static/music/tap.mp3",
+            src: "./static/music/tap.Mp3",
             id: "tap"
         });
         gameRestart();
